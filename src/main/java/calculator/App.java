39ca346e0c17d680032091df93b36555a1979e21
Robbie <robbie331@>
2025-07-09 22:08:27 +0900

    1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,18 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
 
+        // 첫번째 숫자 입력 받기
+        System.out.println("첫 번째 숫자를 입력하세요 :");
+        int num1 = sc.nextInt();
+        // 두번째 숫자 입력 받기
+        System.out.println("두 번째 숫자를 입력하세요 : ");
+        int num2 = sc.nextInt();
     }
 }
