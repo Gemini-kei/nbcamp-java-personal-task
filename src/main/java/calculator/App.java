a3105f9cd504a572728de61ee13f5817175a8a86
Robbie <robbie331@>
2025-07-09 23:01:25 +0900

    4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,37 +8,46 @@ public class App {
         // 입력
         Scanner sc = new Scanner(System.in);
 
-        // 첫번째 숫자 입력 받기
-        System.out.println("첫 번째 숫자를 입력하세요 :");
-        int num1 = sc.nextInt();
-        // 두번째 숫자 입력 받기
-        System.out.println("두 번째 숫자를 입력하세요 : ");
-        int num2 = sc.nextInt();
+        while(true){
+            // 첫번째 숫자 입력 받기
+            System.out.println("첫 번째 숫자를 입력하세요 :");
+            int num1 = sc.nextInt();
+            // 두번째 숫자 입력 받기
+            System.out.println("두 번째 숫자를 입력하세요 : ");
+            int num2 = sc.nextInt();
 
-        //사칙연산 기호 입력 받기
-        System.out.println("사칙연산 기호를 입력하세요 : ");
-        char symbol = sc.next().charAt(0);
+            //사칙연산 기호 입력 받기
+            System.out.println("사칙연산 기호를 입력하세요 : ");
+            char symbol = sc.next().charAt(0);
 
-        int result = 0;
-        switch (symbol) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            int result = 0;
+            switch (symbol) {
+                case '+':
+                    result = num1 + num2;
                     break;
-                }
-                result = num1 / num2;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        break;
+                    }
+                    result = num1 / num2;
+                    break;
+            }
+            System.out.println("결과 : " + result);
+
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String stopMsg = sc.next();
+            if (stopMsg.equals("exit")) {
                 break;
+            }
         }
-        System.out.println("결과 : " + result);
 
     }
 }
