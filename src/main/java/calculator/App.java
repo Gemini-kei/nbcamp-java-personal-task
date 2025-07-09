d2a1d37aae4a51471df58ad72b85331b9d7adc91
Robbie <robbie331@>
2025-07-09 22:56:26 +0900

    3. 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -19,6 +19,26 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요 : ");
         char symbol = sc.next().charAt(0);
 
+        int result = 0;
+        switch (symbol) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = num1 / num2;
+                break;
+        }
+        System.out.println("결과 : " + result);
 
     }
 }
